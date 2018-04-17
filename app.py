f16a0e116b6dfc9e04f484bfe02e868d6cf6150d
kunal <kunal.patel@elevenrobots.com>
2018-04-17 12:24:10 +0100

    updated environment var

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask
-from boto.s3.connection import S3Connection
+
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    username = s3 = S3Connection(os.environ['DB_USER'])
-    return "<h1>Hello heroku test commit " + username +  "</h1>"
+    username = os.environ['DB_USER']
+    return "<h1>Hello heroku test commit username " + username + "</h1>"
     
 
 if __name__ == '__main__':
